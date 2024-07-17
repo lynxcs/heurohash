ec8602be9b88227cb6d89863d65f6888f7888b82
Domas Kalinauskas <domaskal@gmail.com>
2024-07-17 20:46:59 +0300

    remove old

--- old/heurohash_map.hpp
+++
@@ -1,7 +0,0 @@
-// Use quadratic probing, try to maintain 90% load factor
-
-// template<typename KeyT, typename ValueT>
-// class QuadraticMap {
-//     constexpr QuadraticMap(std::initializer_list<std::pair<const KeyT, ValueT>> initial_values) {
-//     }
-// };
--- old/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "heurohash_properties.hpp"
-#include "heurohash_hashfunc.hpp"
-
-int main() {
-    return 0;
-}
